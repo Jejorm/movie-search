07380f80ad52583a21517c550a2ea916a90dc4b5
Jeremy Orellana jejorm8@gmail.com <jejorm8@gmail.com>
2023-04-06 20:08:48 -0500

    vite config modified

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: '/movie-searcher/'
+    base: '/movie-searcher'
   }
 })
